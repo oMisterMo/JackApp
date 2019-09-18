3221dd998c39453a2838e2f9ff5fdffce684879b
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2019-09-18 13:05:24 +0100

    Added simple Constructor

--- app/src/main/java/com/ds/mo/jackapp/Session.java
+++ app/src/main/java/com/ds/mo/jackapp/Session.java
@@ -4,4 +4,15 @@ public class Session {
 
     public String day, month, year, location, price;
 
+    public Session() {
+
+    }
+
+    public Session(String day, String month, String year, String location, String price) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.location = location;
+        this.price = price;
+    }
 }
--- app/src/main/java/com/ds/mo/jackapp/Transaction.java
+++ app/src/main/java/com/ds/mo/jackapp/Transaction.java
@@ -3,4 +3,16 @@ package com.ds.mo.jackapp;
 public class Transaction {
 
     public String day, month, year, name, price;
+
+    public Transaction() {
+
+    }
+
+    public Transaction(String day, String month, String year, String name, String price) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.name = name;
+        this.price = price;
+    }
 }
